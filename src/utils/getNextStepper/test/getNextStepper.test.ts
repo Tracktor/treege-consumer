f1b07c9a7708d2e5a1d3d45cf48fec25be326c81
badreleglil <badre.leglil@gmail.com>
2022-11-07 11:16:12 +0100

    Test: getNextStepper

--- src/utils/getNextStepper/test/getNextStepper.test.ts
+++ src/utils/getNextStepper/test/getNextStepper.test.ts
@@ -1,5 +1,11 @@
 import getNextStepper from "@/utils/getNextStepper";
-import { emptyFields, fieldsWithNextFieldNoHidden } from "@/utils/getNextStepper/test/mock";
+import {
+  emptyFields,
+  fieldsWithMultipleHiddenAndNextHidden,
+  fieldsWithNextFieldHidden,
+  fieldsWithNextFieldNoHidden,
+  fieldsWithTwoNextFieldHidden,
+} from "@/utils/getNextStepper/test/mock";
 
 describe("test get next stepper", () => {
   test("Empty fields Array", () => {
@@ -9,8 +15,29 @@ describe("test get next stepper", () => {
     expect(result).toEqual(output);
   });
 
-  test("Simple rest field", () => {
+  test("Fields with next text field", () => {
     const { fields, output } = fieldsWithNextFieldNoHidden;
+    const result = getNextStepper(fields);
+
+    expect(result).toEqual(output);
+  });
+
+  test("Fields with next field hidden", () => {
+    const { fields, output } = fieldsWithNextFieldHidden;
+    const result = getNextStepper(fields);
+
+    expect(result).toEqual(output);
+  });
+
+  test("Fields with two next field hidden", () => {
+    const { fields, output } = fieldsWithTwoNextFieldHidden;
+    const result = getNextStepper(fields);
+
+    expect(result).toEqual(output);
+  });
+
+  test("Fields with multiple hidden fields and next field hidden", () => {
+    const { fields, output } = fieldsWithMultipleHiddenAndNextHidden;
     const result = getNextStepper(fields);
 
     expect(result).toEqual(output);
